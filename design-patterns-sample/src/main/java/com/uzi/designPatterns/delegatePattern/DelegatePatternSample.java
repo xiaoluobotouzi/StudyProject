899888d8d52d949aff0aa352a7894c59a8851277
kunling <17600356784@163.com>
2020-06-22 22:32:23 +0800

    Java Base 设计模式

---
+++ design-patterns-sample/src/main/java/com/uzi/designPatterns/delegatePattern/DelegatePatternSample.java
@@ -0,0 +1,11 @@
+package com.uzi.designPatterns.delegatePattern;
+
+/**
+ * @ClassName: DelegatePatternSample
+ * @Description: 委派模式 sample
+ * @Author: uziJamesi
+ * @Date 2020/6/18 22:45
+ * ...
+ */
+public class DelegatePatternSample {
+}
