24cf627c86953fc8f946a1139160ed1e5c643c2a
kunLing <kunling.liu@pharmacodia.com>
2021-01-18 18:02:41 +0800

    io sample

--- java-base-sample/src/main/java/com/uzi/javaIo/bio/IOStreamSample.java
+++ java-base-sample/src/main/java/com/uzi/javaIo/bio/IOStreamSample.java
@@ -1,8 +1,6 @@
 package com.uzi.javaIo.bio;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 /**
  * @ClassName: IOStream
@@ -13,20 +11,89 @@ import java.io.OutputStream;
  */
 public class IOStreamSample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+//        byteArrayIOStreamTest();
+
+        fileIOStreamTest();
+
+
 
     }
 
-    private void StreamTest(){
+    private void streamTest() {
 
         InputStream is;
         OutputStream os;
 
     }
 
-    private void byteArrayInputStreamTest(){
-        String ss = "ABC123";
-        InputStream is = new ByteArrayInputStream(ss.getBytes());
+    /**
+     * 文件输入输出流示例
+     */
+    private static void fileIOStreamTest() throws IOException {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            fis = new FileInputStream("/a.txt");
+            fos = new FileOutputStream("/b.txt");
 
+            // 创建一个缓冲字节数组，可减少IO操作，提高读写效率，一般用于大文件操作
+            byte[] byt = new byte[1024];
+
+            // 读取缓冲字节数组长度的字节
+//            int read = fis.read(byt);
+            int read = fis.read();
+            while(read != -1){
+//                fos.write(byt, 0, read);
+                fos.write(read);
+                read = fis.read();
+            }
+            fos.flush();
+        } catch (FileNotFoundException e) {
+            System.out.println("文件不存在");
+        } catch (IOException e) {
+            System.out.println("系统异常");
+        } finally {
+            if(null != fos){
+                fos.close();
+            }
+            if(null != fis){
+                fis.close();
+            }
+        }
+    }
+
+    /**
+     * 字节数组输入输出流示例
+     */
+    private static void byteArrayIOStreamTest() throws IOException {
+        ByteArrayInputStream bis = null;
+        ByteArrayOutputStream bos = null;
+        try {
+            String ss = "Hello World";
+            bis = new ByteArrayInputStream(ss.getBytes());
+            bos = new ByteArrayOutputStream();
+
+            int read = bis.read();
+            while (read != -1) {
+                char c = (char) read;
+                System.out.print(" " + c);
+
+                bos.write(read);
+                read = bis.read();
+            }
+
+            System.out.println();
+
+            System.out.println(bos.toString());
+        } finally {
+            if(null != bos){
+                bos.close();
+            }
+            if(null != bis){
+                bis.close();
+            }
+        }
     }
 }
